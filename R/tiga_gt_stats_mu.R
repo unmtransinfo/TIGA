fc7e353929b56a02808517c058a524308921cda7
Jeremy Yang <jeremyjyang@gmail.com>
2020-07-28 15:12:00 -0600

    Switch to meanrank from mu.

--- R/tiga_gt_stats_mu.R
+++ R/tiga_gt_stats_mu.R
@@ -6,16 +6,28 @@
 #############################################################################
 ### SEE FOR ALL INPUT WORKFLOWS: Go_gwascat_GetData.sh
 #############################################################################
-# Now using avg-rank or median-rank instead of mu_scores.
-# Based on DISEASES benchmark results, using only variables:
-#   * n_study
-#   * pvalue_mlog_median
-#   * rcras
+# Multivariable non-parametric ranking via &mu; scores.
+###
+# Non-dominated solutions are not inferior to any other case at any variable.
+# A &mu; score is defined as the number of lower cases minus the number of higher.
+# The resulting ranking is the useful result, not so much the score itself.
+# Using muStat package.
+###
+# mu.GE: greater than or equal to
+# mu.AND: Logical AND GEs for all variables
+###
+# Issue: cases globally superior in one variable and inferior in one variable
+# have nAbove=0 and nBelow=0 and muScore=0. What should be the rank?
+# If a case has nAbove=0 and nBelow=0 then weight=0 and mu.Sums() returns score = NA.
+# This is arbitrary so we assign score = nBelow = nAbove = 0 - 0 = 0.
+# ?mu.Sums:
+# score  = (nB-nA) * ifelse(weight==0, NA, 1)
 #############################################################################
 # Writes gt_provenance file with TRAIT_URI, ensemblId, STUDY_ACCESSION and PUBMEDID.
 #############################################################################
 library(readr, quietly=T)
 library(data.table, quietly=T)
+library(muStat)
 #
 t_start <- Sys.time()
 #
@@ -354,70 +366,73 @@ message(sprintf("Genes (ensemblIDs): %d", uniqueN(gt_stats$ensemblId)))
 message(sprintf("Genes (symbols): %d", uniqueN(gt_stats$geneSymbol)))
 message(sprintf("Traits in dataset: %d", uniqueN(gt_stats$efoId)))
 message(sprintf("G-T associations in dataset: %d", nrow(gt_stats)))
-###
 
-# Save pre-scored variables to file for debugging.
+###
+# &mu; scores/rankings
+#   * __for a given trait__
+#   * __for a given gene__
+###
+#
+# Use inverse of n\_traits\_g and n\_genes\_t so bigger is better as needed for muStat.
+gt_stats[, geneNtrait_inv := 1 / geneNtrait]
+gt_stats[, traitNgene_inv := 1 / traitNgene]
+
+# Save pre-mu to file for debugging.
 write_delim(gt_stats, "data/tmp.tsv.gz", delim="\t")
 
+# For each (trait|gene), convert to matrix for muStat::mu.GE().
+# The (i,j) entry of GE matrix is 1 if \code{x_i >= x_j}, 0 otherwise.
+# The square matrix GE is stored by column in a vector. Thus nrow(GE_matrix) = nrow(x)^2.
 ###
-# Mean-rank computation. For each variable in defined set, 
-# compute rank with ties having same rank, then compute mean of 
-# variable-ranks.
-# base::rank cannot equate NAs so need custom my_rank().
-# Best rank is 1. All variables should be bigger is better.
-###
-my_rank <- function(v) {
-  ranks <- base::rank(v, na.last="keep", ties.method="min")
-  rank_max <- max(ranks, na.rm=T)
-  ranks <- ifelse(is.na(ranks), rank_max+1, ranks)
-  return(ranks)
-}
-###
-TAGS_FOR_RANKING <- c("n_study", "pvalue_mlog_median", "rcras")
-###
-# Gene meanrank:
-gt_stats[, geneMeanRank := as.numeric(NA)]
+# Gene &mu; scores:
+# Some or_median will be NA, since beta included.
+gt_stats[, `:=`(geneMuScore=as.integer(NA), geneMuRank=as.integer(NA))]
 ii <- 0
 for (efoId_this in unique(gt_stats$efoId)) {
+  gtmat <- as.matrix(gt_stats[efoId==efoId_this, .(n_study, n_snp, n_snpw, geneNtrait_inv, traitNgene_inv, pvalue_mlog_median, or_median, n_beta, rcras)])
   ii <- ii + 1
   trait_this <- gt_stats[efoId==efoId_this, trait][1]
-  n_gene_this <- gt_stats[efoId==efoId_this, uniqueN(ensemblId)]
-  message(sprintf("[%d / %d] (N_gene: %3d) %s:\"%s\"", ii, uniqueN(gt_stats$efoId), n_gene_this, efoId_this, trait_this))
-  if (n_gene_this<2) { #singletons
-    gt_stats[efoId==efoId_this]$geneMeanRank <- 1
+  message(sprintf("[%d / %d] (N_gene: %3d) %s:\"%s\"", ii, uniqueN(gt_stats$efoId), dim(gtmat)[1], efoId_this, trait_this))
+  if (dim(gtmat)[1]<2) { #Singletons
+    gt_stats[efoId==efoId_this]$geneMuScore <- 0
+    gt_stats[efoId==efoId_this]$geneMuRank <- 1
     next;
   }
-  ranks_this <- list()
-  for (tag in TAGS_FOR_RANKING) {
-    ranks_this[[tag]] <- my_rank(-gt_stats[efoId==efoId_this][[tag]])
-  }
-  setDT(ranks_this)
-  ranks_this[, meanRank := rowMeans(.SD)]
-  gt_stats[efoId==efoId_this]$geneMeanRank <- ranks_this$meanRank
+  ge <- mu.GE(gtmat)
+  sums <- mu.Sums(mu.AND(ge)) # Logical AND GEs for all variables 
+  setDT(sums)
+  sums$name <- gt_stats[efoId==efoId_this, .(geneName)]
+  sums[is.na(score) & weight==0, score := 0] # Override NA scores
+  sums[order(-score, -weight), rank := 1:nrow(sums)]
+  gt_stats[efoId==efoId_this]$geneMuScore <- sums$score
+  gt_stats[efoId==efoId_this]$geneMuRank <- sums$rank
 }
 #
 ###
-# Trait meanrank:
-gt_stats[, traitMeanRank := as.numeric(NA)]
+# Trait &mu; scores:
+gt_stats[, `:=`(traitMuScore=as.integer(NA), traitMuRank=as.integer(NA))]
 ii <- 0
 for (ensemblId_this in unique(gt_stats$ensemblId)) {
-  n_trait_this <- gt_stats[ensemblId==ensemblId_this, uniqueN(efoId)]
+  gtmat <- as.matrix(gt_stats[ensemblId==ensemblId_this, .(n_study, n_snp, n_snpw, geneNtrait_inv, traitNgene_inv, pvalue_mlog_median, or_median, n_beta, rcras)])
   ii <- ii + 1
   geneSymbol_this <- gt_stats[ensemblId==ensemblId_this, geneSymbol][1]
-  message(sprintf("[%d / %d] (N_trait: %3d) %s:\"%s\"", ii, uniqueN(gt_stats$ensemblId), n_trait_this, ensemblId_this, geneSymbol_this))
-  if (n_trait_this<2) { #singletons
-    gt_stats[ensemblId==ensemblId_this]$traitMeanRank <- 1
+  message(sprintf("[%d / %d] (N_trait: %3d) %s:\"%s\"", ii, uniqueN(gt_stats$ensemblId), dim(gtmat)[1], ensemblId_this, geneSymbol_this))
+  if (dim(gtmat)[1]<2) { #Singletons
+    gt_stats[ensemblId==ensemblId_this]$traitMuScore <- 0
+    gt_stats[ensemblId==ensemblId_this]$traitMuRank <- 1
     next;
   }
-  ranks_this <- list()
-  for (tag in TAGS_FOR_RANKING) {
-    ranks_this[[tag]] <- my_rank(-gt_stats[ensemblId==ensemblId_this][[tag]])
-  }
-  setDT(ranks_this)
-  ranks_this[, meanRank := rowMeans(.SD)]
-  gt_stats[ensemblId==ensemblId_this]$traitMeanRank <- ranks_this$meanRank
+  ge <- mu.GE(gtmat)
+  sums <- mu.Sums(mu.AND(ge)) # Logical AND GEs for all variables 
+  setDT(sums)
+  sums$name <- gt_stats[ensemblId==ensemblId_this, .(geneName)]
+  sums[is.na(score) & weight==0, score := 0] # Override NA scores
+  sums[order(-score, -weight), rank := 1:nrow(sums)]
+  gt_stats[ensemblId==ensemblId_this]$traitMuScore <- sums$score
+  gt_stats[ensemblId==ensemblId_this]$traitMuRank <- sums$rank
 }
 ###
+gt_stats[, `:=`(geneNtrait_inv = NULL, traitNgene_inv = NULL)]
 #
 write_delim(gt_stats, ofile, delim="\t")
 writeLines(sprintf("Output file written: %s", ofile))
