080494b09c8666d7eb5b40f6d5a62bd64875fcb6
Jeremy Yang <jeremyjyang@gmail.com>
2022-07-13 14:32:08 -0600

    Alright.

--- sh/Go_efo_Grouping.sh
+++ sh/Go_efo_Grouping.sh
@@ -6,45 +6,78 @@ cwd=$(pwd)
 #
 DATADIR=${cwd}/data
 #
-EFO_DIR="$HOME/../data/EFO/data"
+EFO_DIR="$(cd $HOME/../data; pwd)/EFO/data"
 OWLFILE="$EFO_DIR/efo.owl"
 #
-LIBDIR="$HOME/../app/lib"
+LIBDIR="$(cd $HOME/../app; pwd)/lib"
+#
+# GWASCatalog release:
+if [ $# -eq 1 ]; then
+	GC_REL=$1
+else
+	printf "ERROR: syntax $(basename $0) \"YYYY-MM-DD\"\n"
+	exit
+fi
+#
+GC_REL_Y=$(echo $GC_REL |sed 's/-.*$//')
+GC_REL_M=$(echo $GC_REL |sed 's/^.*-\(.*\)-.*$/\1/')
+GC_REL_D=$(echo $GC_REL |sed 's/^.*-//')
+#
+ODIR="${DATADIR}/${GC_REL_Y}${GC_REL_M}${GC_REL_D}"
+printf "Output dir: ${ODIR}\n"
+#
+###
+EFO_URL="https://github.com/EBISPOT/efo/releases/download/v${EFO_RELEASE}/efo.owl"
+wget -q -O $OWLFILE $EFO_URL
+#
+#############################################################################
+EFO_RELEASE="3.40.0"
+printf "${EFO_RELEASE}\n" >${ODIR}/efo_release.txt
+#
+#############################################################################
+#
+java -jar $LIBDIR/iu_idsl_jena-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
+	-ifile_ont ${OWLFILE} -vv -ont2tsv -o ${ODIR}/efo.tsv
+#
+# From efo.tsv create GraphML file:
+graphmlfile="${ODIR}/efo_graph.graphml"
+${cwd}/R/efo_graph.R $GC_REL_Y $GC_REL_M $GC_REL_D
+gzip -f ${graphmlfile}
 #
 #############################################################################
 # Exports:
 ###
 # CYJS: Not needed now but maybe later?
 java -jar $LIBDIR/iu_idsl_jena-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
-	-ifile_ont ${OWLFILE} -vv -ont2cyjs -o ${DATADIR}/efo.cyjs
+	-ifile_ont ${OWLFILE} -vv -ont2cyjs -o ${ODIR}/efo.cyjs
 ###
 # Edgelist and nodelist
 java -jar $LIBDIR/iu_idsl_jena-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
-	-ifile_ont ${OWLFILE} -vv -ont2edgelist -o ${DATADIR}/efo_edgelist.tsv
+	-ifile_ont ${OWLFILE} -vv -ont2edgelist -o ${ODIR}/efo_edgelist.tsv
 java -jar $LIBDIR/iu_idsl_jena-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
-	-ifile_ont ${OWLFILE} -vv -ont2nodelist -o ${DATADIR}/efo_nodelist.tsv
+	-ifile_ont ${OWLFILE} -vv -ont2nodelist -o ${ODIR}/efo_nodelist.tsv
 #
 #############################################################################
 ###
 # Grouping:
-cat $DATADIR/gwascat_trait.tsv \
+cat $ODIR/gwascat_trait.tsv \
 	|sed -e '1d' |awk -F '\t' '{print $5}' |sort -u \
-	>$DATADIR/gwascatalog.efoid
-printf "EFO IDs: %d\n" $(cat $DATADIR/gwascatalog.efoid |grep '^EFO' |wc -l)
+	>$ODIR/gwascatalog.efoid
+printf "EFO IDs: %d\n" $(cat $ODIR/gwascatalog.efoid |grep '^EFO' |wc -l)
 ###
 ${cwd}/python/nx_analysis.py cluster --min_groupsize 2 --max_level 10 \
-	--i_edge $DATADIR/efo_edgelist.tsv \
-	--i_node_attr $DATADIR/efo_nodelist.tsv \
-	--i_node_set $DATADIR/gwascatalog.efoid \
+	--i_edge $ODIR/efo_edgelist.tsv \
+	--i_node_attr $ODIR/efo_nodelist.tsv \
+	--i_node_set $ODIR/gwascatalog.efoid \
 	--setname gwc --graphname "EFO" \
-	--o $DATADIR/efo_groups.tsv
+	--o $ODIR/efo_groups.tsv
 #
 #
 ${cwd}/R/efo_groups.R \
 	"mood disorder" \
-	$DATADIR/efo.tsv \
-	$DATADIR/efo_groups.tsv \
-	$DATADIR/efo_subgraph.graphml \
+	$ODIR/efo.tsv \
+	$ODIR/efo_groups.tsv \
+	$ODIR/efo_subgraph.graphml \
 ###
 # To do:
 #   * Groups file usable by TIGA app for marker color and selection.
