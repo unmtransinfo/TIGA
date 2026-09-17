eadb576b514d35bc1ea4499435c9b52082c08d26
Jeremy Yang <jeremyjyang@gmail.com>
2026-09-17 15:12:15 -0600

    Ok.

--- sh/cyverse/cyverse_init_tiga.sh
+++ sh/cyverse/cyverse_init_tiga.sh
@@ -8,20 +8,30 @@ date
 ###
 # SRCDATADIR="/home/kasm-user/data-store/data/swcactiZone/home/jjyang/analyses/GWASCatalog/releases/2026/07/10/"
 ln -s $HOME/data-store/data/swcactiZone/home/jjyang/analyses $HOME/data
-###
-$HOME/data/cyverse_init_tiga_r.sh >& $HOME/cyverse_init_tiga_r_${TIMESTAMP}.log
-cp $HOME/cyverse_init_tiga_r_${TIMESTAMP}.log $HOME/data-store/data/output/
-###
 #
+WORKDIR="$HOME/data"
 ###
-$HOME/data/cyverse_init_tiga_py.sh >& $HOME/cyverse_init_tiga_py_${TIMESTAMP}.log
-cp $HOME/cyverse_init_tiga_py_${TIMESTAMP}.log $HOME/data-store/data/output/
+SCRIPT="cyverse_init_tiga_r.sh"
+LOG="${SCRIPT}-${TIMESTAMP}.log"
+setsid nohup $WORKDIR/$SCRIPT >$WORKDIR/$LOG 2>&1 & disown
+echo "$SCRIPT started in background (pid $!); tail -f $WORKDIR/$LOG to watch progress"
+#cp $WORKDIR/$LOG $HOME/data-store/data/output/
+###
+sleep 3
+###
+SCRIPT="cyverse_init_tiga_py.sh"
+LOG="${SCRIPT}-${TIMESTAMP}.log"
+setsid nohup $WORKDIR/$SCRIPT >$WORKDIR/$LOG 2>&1 & disown
+echo "$SCRIPT started in background (pid $!); tail -f $WORKDIR/$LOG to watch progress"
+#cp $WORKDIR/$LOG $HOME/data-store/data/output/
+###
+sleep 3
 ###
 git clone https://github.com/unmtransinfo/TIGA.git
 #
-cd TIGA
-./sh/Go_TIGA_Workflow.sh >& $HOME/Go_TIGA_Workflow_${TIMESTAMP}.log
-cp $HOME/Go_TIGA_Workflow_${TIMESTAMP}.log $HOME/data-store/data/output/
+#cd TIGA
+#./sh/Go_TIGA_Workflow.sh >& $HOME/Go_TIGA_Workflow_${TIMESTAMP}.log
+#cp $HOME/Go_TIGA_Workflow_${TIMESTAMP}.log $HOME/data-store/data/output/
 ###
 date
 printf "DONE (${SCRIPTNAME})\n"
--- sh/cyverse/cyverse_init_tiga_r.sh
+++ sh/cyverse/cyverse_init_tiga_r.sh
@@ -6,8 +6,15 @@ SCRIPTNAME="$(basename $0)"
 date
 ###
 sudo apt update
+#
+# Dependency for shiny:
+sudo apt install -y libuv1-dev
+# Dependency for plotly:
+sudo apt install -y libssl-dev
+sudo apt install -y libcurl4-openssl-dev
+#
 sudo apt install -y r-base-dev
-sudo R -e 'install.packages(c("readr", "data.table", "igraph", "devtools"))'
+sudo R -e 'install.packages(c("readr", "data.table", "igraph", "shiny", "DT", "shinyBS", "tableHTML", "plotly"))'
 #
 ###
 date
